918d739658936adca66828a2db54b24fb41fe265
dostonshernazarov <dostonshernazarov2001@gmail.com>
2024-06-04 23:57:29 +0500

    add network to ci/cd

---
+++ mock.sql
@@ -0,0 +1,11 @@
+INSERT INTO animals (id, name, category_name, gender, birth_day, genus, weight, description, is_health, created_at, updated_at) VALUES
+('550e8400-e29b-41d4-a716-446655440000', 'test1', 'sheep', 'male', '2015-08-15', 'genus1', 30.5, 'King of the Jungle', 'true', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440001', 'test2', 'goat', 'female', '2010-04-12', 'genus2', 21.0, 'Largest land animal', 'false', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440002', 'test3', 'horse', 'male', '2018-06-01', 'genus3', 130.0, 'Flightless bird', 'true', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440003', 'test4', 'cow', 'female', '2012-09-22', 'genus4', 450.0, 'Large aquatic reptile', 'true', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440004', 'test5', 'sheep', 'female', '2016-11-30', 'genus5', 80.0, 'Tallest land animal', 'true', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440005', 'test6', 'cow', 'male', '2019-01-05', 'genus4', 140.0, 'Herbivorous marsupial', 'true', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440006', 'test7', 'cow', 'female', '2020-03-15', 'genus3', 300.9, 'Colorful bird', 'false', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440007', 'test8', 'horse', 'male', '2013-07-21', 'genus1', 200.0, 'Hard-shelled reptile', 'true', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440008', 'test9', 'goat', 'female', '2017-02-14', 'genus5', 35.0, 'Large marsupial', 'true', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('550e8400-e29b-41d4-a716-446655440009', 'test10', 'sheep', 'male', '2011-12-09', 'genus7', 36.5, 'Bird of prey', 'false', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
